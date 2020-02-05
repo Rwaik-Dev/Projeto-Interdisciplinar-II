ec8cba125473fe7f2c46b349f9c72802ee4ce0a5
Wilker Rwaik Eloi <60619715+Rwaik-Dev@users.noreply.github.com>
2020-02-05 10:58:11 -0300

    Add files via upload

--- cadastro.php
+++ cadastro.php
@@ -1,30 +1,42 @@
 <?php
 
 $login = $_POST['login']; //Atribui $login ao q foi digitado no campo de login la na tela cadastro.html.
-$senha = MD5($_POST['senha']); // Atribui $senha ao que foi digitado no campo senha em cadastro.html.
-$connect = mysql_connect(127.0.0.1,'root',''); // Abre conexão com BD.
-$db = mysql_select_db('PI_002'); // Seleciona o BD.
-$query_select = "SELECT login FROM usuarios WHERE login = '$login'"; // Inicio da verificação de usuario.
-$select = mysql_query($query_select,$connect);
-$array = mysql_fetch_array($select);
+$senha = $_POST['senha']; // Atribui $senha ao que foi digitado no campo senha em cadastro.html.
+//$connect = mysqli_connect('127.0.0.1','root',''); // Abre conexão com BD.
+$sql["host"] = "localhost";
+$sql["usuario"] = "root";
+$sql["senha"] = "";
+$sql["base"] = "pi_002";
+$connect = mysqli_connect($sql["host"],$sql["usuario"],$sql["senha"],$sql["base"]); // Abre conexão com BD.
+$db = mysqli_select_db($connect,'pi_002'); // Seleciona o BD.
+$query_select = "SELECT login FROM usuarios WHERE login = '$login' "; // Inicio da verificação de usuario.
+$select = mysqli_query($connect,$query_select);
+$array = mysqli_fetch_array($select);
 $logarray = $array['login'];
 
-if ($login == "" || $login == null) { // Situação que verifica se o campo login esta vazio.
+if ($login == "" || $login == null)
+{ // Situação que verifica se o campo login esta vazio.
   echo "<script language='javascript' type='text/javascript'>
-  alert('O campo login deve ser preenchido');window.location.href='cadastro.html';</script>"
-}else {
-  if ($logarray == $login) { // Situação que verifica se o usuario esta cadastrado no BD.
+  alert('O campo login deve ser preenchido');window.location.href='cadastro.html';</script>";
+}
+else {
+  if ($logarray == $login)
+  { // Situação que verifica se o usuario esta cadastrado no BD.
     echo "<script language= 'javascript' type= 'text/javascript'>
     alert('Esse login já existe');window.location.href='cadastro.html';</script>";
     die();
-  }else{ // Caso não esteja, nesta situação o sistema cadastra o mesmo no sistema.
+  }
+  else{ // Caso não esteja, nesta situação o sistema cadastra o mesmo no sistema.
     $query = "INSERT INTO usuarios (login,senha) VALUES ('$login','$senha')";
-    $insert = mysql_query($query,$connect);
+    $insert = mysqli_query($connect,$query);
 
-    if ($insert) { // Situação que retorna se o cadastro foi realizado com exito.
+    if ($insert)
+    { // Situação que retorna se o cadastro foi realizado com exito.
       echo "<script language='javascript' type= 'text/javascript'>
       alert('Usuário cadastrado com sucesso!');window.location.href='login.html'</script>";
-    }else{ // Caso contrario ele retorna uma mensagem que não foi posivel cadastrar o msm.
+    }
+    else
+    { // Caso contrario ele retorna uma mensagem que não foi posivel cadastrar o msm.
       echo"<script language='javascript' type='text/javascript'>
       alert('Não foi possivel cadastrar esse usuario.')window.location.href='cadastro.html'</script>";
     }
--- login.php
+++ login.php
@@ -2,15 +2,19 @@
 
 $login= $_POST['login'];
 $entrar = $_POST['entrar'];
-$senha = md5($_POST['senha']);
-$connect = mysql_connect('127.0.0.1','root','');
-$db = mysql_select_db('PI_002');
+$senha = $_POST['senha'];
+$connect = mysqli_connect('localhost','root','');
+$db = mysqli_select_db($connect,'PI_002');
 if (isset($entrar)) {
 
-  $verifica = mysql_query("SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha'") or die("Erro ao selecionar")
-  if (mysql_num_rows($verifica) <= 0) {
+  $verifica = mysqli_query($connect,"SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha' ") or die("Erro ao selecionar");
+
+
+  if (mysqli_num_rows($verifica) <= 0) {
     echo "<script language= 'javascript' type= 'text/javascript'>
     alert('Login e/ou senha incorretos.');window.location.href='login.html';</script>";
+    echo $senha;
+
     die();
   }else{
     setcookie("login",$login);
